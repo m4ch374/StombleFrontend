e4f8c1e32660bae1156996146b516b529c878d77
m4ch374 <82320948+m4ch374@users.noreply.github.com>
2023-08-10 08:54:16 +1000

    fix(setup_password): fixed, waiting for integration (#18)
    * fix(setup_password): fixed, waiting for integration
    
    * fix(msg): password match feedback

--- src/screens/authentication/SignUp/SetUpPassword.tsx
+++ src/screens/authentication/SignUp/SetUpPassword.tsx
@@ -1,214 +1,120 @@
-import { View, Text, TextInput, TouchableWithoutFeedback, Keyboard } from 'react-native'
-import React, { useState } from 'react'
-import { Formik } from 'formik'
-import { Ionicons, EvilIcons } from '@expo/vector-icons'
-import * as Yup from 'yup'
-import { Link } from '@react-navigation/native'
-import { NativeStackNavigationProp } from '@react-navigation/native-stack'
+import { View, Text, TouchableWithoutFeedback, Keyboard } from 'react-native'
+import React, { useMemo, useState } from 'react'
+import { useNavigation } from '@react-navigation/native'
 import FlatButton from '../../../components/styled_components/FlatButton'
-import LoginScreenLayout from '../../../components/styled_components/LoginScreenLayout'
-import { AuthStackList } from '../../../types/Navigation'
 import BackgroundColour from '../../../components/styled_components/BackgroundColour'
+import PasswordInput from '../../../components/passwordInput'
+import { useAppDispatch } from '../../../redux/hooks'
+import { tmpStoreAction } from '../../../redux/reducers/tmpStore.reducer'
 
-interface Props {
-  navigation: NativeStackNavigationProp<AuthStackList, 'SetUpPassword'>;
+// Breaking the rules here a bit
+type TPasswordCheck = {
+  children: string
+  criteria: () => boolean
 }
-// const passwordReg=Yup.object({
-//   newPassword:Yup.string()
-//   .required()
-//   .matches(
-//     /^(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$/,
-//     'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character',
-//   )
 
-//   ,
-//   confirmPW:Yup.string()
-//   .required('Confirm password is required')
-//   .oneOf([Yup.ref('newPassword')], 'Passwords must match'),
-// })
+const PasswordCheck: React.FC<TPasswordCheck> = ({ children, criteria }) => {
+  return (
+    <Text className={`${criteria() ? "text-green-500/90" : "text-[#ABABAB]"} text-[14px]`}>
+      <Text>{criteria() ? "✔ " : "• "}</Text>
+      <Text>{children}</Text>
+    </Text>
+  )
+}
 
-const SetUpPassword = ({ navigation }: Props) => {
-  const [secureTextEntry, setSecureTextEntry] = useState(false)
-  //const password = "MyPassword123!"; // Replace this with the actual password value
-  //const isValidPassword = password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$/);
+const SetUpPassword = () => {
+  const navigation = useNavigation()
+  const dispatch = useAppDispatch()
+
+  const [password, setPassword] = useState('')
+  const [confirm, setConfirm] = useState('')
+
+  // Regex criteria collection
+  const regexpCollection = {
+    charNum: /.{8,}/,
+    hasUpper: /[A-Z]+/,
+    hasLower: /[a-z]+/,
+    hasNum: /[0-9]+/,
+    hasSpecChar: /[^a-zA-Z0-9]+/ // I guess anything thats not alphanumerical are special chars (?)
+  } as const
+
+  const testRegexp = (regexp: RegExp) => regexp.test(password)
+  const allMatches = useMemo(() => {
+    return Object.values(regexpCollection).reduce((prev, curr) => {
+      return prev && testRegexp(curr) // some funny brain stuff
+    }, true)
+  }, [password])
+
   return (
     <BackgroundColour> 
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-
-      <View className='p-[16px]'>
-          <View className='mb-[24px]'>
-            <Text className='text-white text-[16px]' style={{ fontFamily: 'Lato-400' }}>
-              Set up a password for extra security.
-            </Text>
-          </View>
+        <View className="px-4 pb-10 flex justify-between h-full">
           <View>
-            <Formik
-              initialValues={{ newPassword: '', confirmPW: '' }}
-              //validationSchema={passwordReg}
-              onSubmit={value => console.log(value)}>
-              {(props) => (
-                <>
-                  <View className='mb-[8px]'>
-                    <Text className='text-[12px] text-[#FFFFFF60]'
-                      style={{ fontFamily: 'Lato-400' }}>
-                      New Password
-                    </Text>
-                  </View> 
-                  <View className='flex-row h-[42px] bg-[#4F4F4F] mb-[24px] rounded-[5px] items-center justify-between px-[12px]'>
-                    <TextInput className='text-white text-[16px] h-full'
-                      style={{ fontFamily: 'Lato-700' }}
-                      placeholder='Add New Password'
-                      placeholderTextColor="#ABABAB"
-                      onChangeText={props.handleChange('newPassword')}
-                      value={props.values.newPassword}
-                      onBlur={props.handleBlur('newPassword')}
-                      autoFocus
-                      secureTextEntry={secureTextEntry}
-                    />
-                    <TouchableWithoutFeedback onPress={() => setSecureTextEntry(!secureTextEntry)}>
-                      <Ionicons
-                        name={secureTextEntry ? "eye-off-outline" : "eye-outline"}
-                        size={20}
-                        color="#C1C1C1"
-                      />
-                    </TouchableWithoutFeedback>
-                  </View>
+            <View className='mb-[24px]'>
+              <Text className='text-white text-[16px] font-semibold' style={{ fontFamily: 'Lato-400' }}>
+                Set up a password for extra security.
+              </Text>
+            </View>
 
-                  {props.initialTouched.newPassword ? null
-                    // <View className='flex-row gap-[5px]'
-                    // style={{marginBottom:props.errors.newPassword? 15:0}}>
-                    //   <Text className='text-[#F4222F] text-[14px] pr-[16px]'>
-                    //     <EvilIcons name="exclamation" size={24} color="#F4222F" />
-                    //     {props.touched.newPassword && props.errors.newPassword}
-                    //   </Text>
-                    // </View>
-                    :
-                    <View className=''
-                      style={{ marginBottom: props.errors.newPassword ? 15 : 0 }}>
-                      <Text className='text-[#ABABAB] text-[14px] pr-[20px]' style={{ marginBottom: 7 }}>
-                        Password must contain:
-                      </Text>
-                      {props.errors.newPassword &&
-                        <View className='flex-row gap-[6px]'>
-                          <EvilIcons name="exclamation" size={24} color="#F4222F" />
-                          <Text className='text-[#F4222F] text-[14px] pr-[22px]' style={{ marginBottom: 10 }} >
-                            {props.errors.newPassword}
-                          </Text>
-                        </View>}
-                      <Text
-                        className={`text-[#ABABAB] text-[14px] pr-[20px]`}
-                        style={{ marginBottom: 5 }}>
-                        At least 8 characters
-                      </Text>
-                      <Text
-                        className={`text-[#ABABAB] text-[14px] pr-[20px]`}
-                        style={{ marginBottom: 5 }}>
-                        An uppercase character
-                      </Text>
-                      <Text
-                        className={`text-[#ABABAB] text-[14px] pr-[20px]`}
-                        style={{ marginBottom: 5 }}>
-                        A lowercase character
-                      </Text>
-                      <Text
-                        className={`text-[#ABABAB] text-[14px] pr-[20px]`}
-                        style={{ marginBottom: 5 }}
-                      >
-                        A number
-                      </Text>
-                      <Text
-                        className={`text-[#ABABAB] text-[14px] pr-[20px]`}
-                        style={{ marginBottom: 5 }}>
-                        A special character
-                      </Text>
-                    </View>
-                  }
+            <View className='mb-[8px]'>
+              <Text className='text-[12px] text-[#FFFFFF60]'
+                style={{ fontFamily: 'Lato-400' }}>
+                New Password
+              </Text>
+            </View> 
+            
+            <PasswordInput password={password} setPassword={setPassword} />
 
-                  <View className='mb-[8px]'>
-                    <Text className='text-[12px] text-[#FFFFFF60]' style={{ fontFamily: 'Lato-400' }}>
-                      Confirm New Password
-                    </Text>
-                  </View>
+            <Text className="text-[#ABABAB] text-[14px]">
+              Password must contain:
+            </Text>
 
-                  <View className='h-[42px] bg-[#4F4F4F] mb-[24px] rounded-[5px] flex-row items-center justify-between px-[12px]'>
-                    <TextInput className='text-white text-[16px] h-full'
-                      style={{ fontFamily: 'Lato-700' }}
-                      placeholder='Confirm Password'
-                      placeholderTextColor="#ABABAB"
-                      onChangeText={props.handleChange('confirmPW')}
-                      value={props.values.confirmPW}
-                      onBlur={props.handleBlur('confirmPW')}
-                      secureTextEntry={secureTextEntry}
-                    />
-                    <TouchableWithoutFeedback onPress={() => setSecureTextEntry(!secureTextEntry)}>
-                      <Ionicons
-                        name={secureTextEntry ? "eye-off-outline" : "eye-outline"}
-                        size={20}
-                        color="#C1C1C1" />
-                    </TouchableWithoutFeedback>
+            {/* 
+              Uhhhhhh...... in hindsight i shouldve just mapped them with an array or something
+              But im too lazy for that now im in the middle of developing it
+              We could always go back and fix right..... ? - time capsule (9/8/2023)
+            */}
+            <View className='flex pl-4 pb-10'>
+              <PasswordCheck criteria={() => { return testRegexp(regexpCollection.charNum) }}>
+                At least 8 characters
+              </PasswordCheck>
+              <PasswordCheck criteria={() => { return testRegexp(regexpCollection.hasUpper) }}>
+                An uppercase character
+              </PasswordCheck>
+              <PasswordCheck criteria={() => { return testRegexp(regexpCollection.hasLower) }}>
+                A lowercase character
+              </PasswordCheck>
+              <PasswordCheck criteria={() => { return testRegexp(regexpCollection.hasNum) }}>
+                A number
+              </PasswordCheck>
+              <PasswordCheck criteria={() => { return testRegexp(regexpCollection.hasSpecChar) }}>
+                A special character
+              </PasswordCheck>
+            </View>
 
-                  </View>
-                  <Text
-                    className={`text-[#ABABAB] text-[14px] pr-[20px]`}
-                    style={{ marginBottom: 5 }}>
-                    Password match
-                  </Text>
+            <View className='mb-[8px]'>
+              <Text className='text-[12px] text-[#FFFFFF60]' style={{ fontFamily: 'Lato-400' }}>
+                Confirm New Password
+              </Text>
+            </View>
 
-                  {props.initialTouched.confirmPW ? null
-                    // <View className='flex-row gap-[5px]'
-                    // style={{marginBottom:props.errors.confirmPW? 15:0}}>
-                    //   <Text className='text-[#F4222F] text-[14px] '>
-                    //     <EvilIcons name="exclamation" size={24} color="#F4222F" />
-                    //     {props.touched.confirmPW && props.errors.confirmPW}
-                    //   </Text>
-                    // </View>
-                    :
-                    <View style={{ marginBottom: props.errors.confirmPW ? 15 : 0 }}>
-                      {props.errors.confirmPW &&
-                        <View className='flex-row gap-[5px]'>
-                          <EvilIcons name="exclamation" size={24} color="#F4222F" />
-                          <Text className='text-[#F4222F] text-[14px]'>
-                            {props.errors.confirmPW}
-                          </Text>
-                        </View>}
-                    </View>
-                  }
+            <PasswordInput password={confirm} setPassword={setConfirm} />
 
-                 <View className='flex-row justify-center items-center mb-[16px] mt-[20px]'>
-                    <Text className='text-[#C1C1C1] text-[10px]'>
-                      By continuing you agree to the
-                    </Text>
-
-                    <View className='px-[4px]'>
-                      <Text className='text-[#326FCB] text-[10px]'>
-                        <Link to={'TermsAndPrivacy'} >
-                          Terms of Service
-                        </Link>
-                      </Text>
-                    </View>
-
-                    <Text className='text-[#C1C1C1] text-[10px]'>
-                      and
-                    </Text>
-
-                    <View className='pl-[4px]'>
-                      <Text className='text-[#326FCB] text-[10px]'>
-                        <Link to={'TermsAndPrivacy'}>
-                          Privacy Policies
-                        </Link>
-                      </Text>
-                    </View>
-                  </View>
-
-                  <FlatButton
-                    text='SEND CODE'
-                    disabled={Object.keys(props.errors).length !== 0}
-                    onPress={() => navigation.navigate('VerifyCode')}
-                  />
-                </>
-              )}
-            </Formik>
+            { password === confirm &&
+              <Text className="text-[#ABABAB] text-[14px] text-green-500/90">✔ Password match</Text>
+            }
+            
           </View>
+
+          <FlatButton
+            text={"VERIFY CODE"}
+            disabled={password !== confirm || !allMatches}
+            onPress={() => { 
+              // TODO: i guess the pre-sign-up thing if i remember correctly? idk
+              dispatch(tmpStoreAction.setItem({ key: "password", item: confirm }))
+              navigation.navigate("Auth", { screen: "VerifyCode" }) 
+            }}
+          />
         </View>
       </TouchableWithoutFeedback>
     </BackgroundColour>
