fa195cd5c8e0d9b8aeb0c16dd1f758ee3a0a5658
YumeLoL <71587684+YumeLoL@users.noreply.github.com>
2023-09-01 11:28:48 +1000

    fix(img upload): fix img upload fail for large file issue (#54)

--- src/components/ChangeProfileModal.tsx
+++ src/components/ChangeProfileModal.tsx
@@ -4,11 +4,10 @@ import React from "react"
 import SwipableModal from "./styled_components/SwipableModal"
 import ModalSettingsBtn from "./profile/ModalSettingsBtn"
 import * as ImagePicker from "expo-image-picker"
-import Fetcher from "utils/Fetcher"
-import { TUpdateIcon } from "types/endpoints"
-import { accountEP } from "constants/Endpoint"
 import { useAppDispatch, useAppSlector } from "redux/hooks"
 import { tmpStoreAction } from "redux/reducers/tmpStore.reducer"
+import { updateIcon } from "utils/services/accountInfo"
+import { TUpdateIcon } from "types/endpoints"
 
 type TChangeProfileModal = {
   stateController: [boolean, React.Dispatch<React.SetStateAction<boolean>>]
@@ -27,49 +26,31 @@ const ChangeProfileModal: React.FC<TChangeProfileModal> = ({
 
   const handleChoosePhoto = (): void => {
     ;(async () => {
-      // TODO: need user permission access to photo?
+      // TODO: need user permission access to photo
       const result = await ImagePicker.launchImageLibraryAsync({
         mediaTypes: ImagePicker.MediaTypeOptions.Images,
         allowsEditing: true,
-        aspect: [4, 3],
-        quality: 1,
+        aspect: [1, 1],
+        quality: 0.3,
         base64: true,
       })
 
-      if (result.canceled) {
-        console.log("Image selection cancelled")
-        return
-      }
+      if (result.canceled) return
 
-      // ----------------------- ISSUE -----------------------
-      // Unable to upload image with a large size
-      // Possible Reason: the base64 string is too big
-      // ( base64 encoded image roughly 33% bigger than the original and AWS Lambda may has a request limit in size and pixels)
-      // Solution: looking for ways to resize before upload (next sprint task)
-      // -----------------------------------------------------
-      console.log("the size of image:", result.assets[0].fileSize)
-
-      const file = {
-        base64: result.assets[0].base64,
-        name: result.assets[0].fileName,
-        type: result.assets[0].type,
-        size: result.assets[0].fileSize?.toString(),
-        ext: result.assets[0].fileName?.split(".").pop(),
-      }
+      const payload = {
+        iconFile: {
+          base64: result.assets[0].base64,
+          name: result.assets[0].fileName,
+          type: "image",
+          size: result.assets[0].fileSize?.toString(),
+          ext: result.assets[0].uri.split(".").pop(),
+        },
+        userId: tmpUser.userId,
+      } as TUpdateIcon["requestType"]
 
       // endpoint: upload photo to backend
-      const resp = await Fetcher.init<TUpdateIcon>(
-        "POST",
-        accountEP.UPDATE_ICON,
-      )
-        .withJsonPaylad({
-          iconFile: file,
-          userId: tmpUser.userId,
-        })
-        .withCurrentToken()
-        .fetchData()
+      const resp = await updateIcon(payload)
 
-      console.log("is img uploaded?", resp)
       if (typeof resp === "undefined") return
 
       dispatch(
